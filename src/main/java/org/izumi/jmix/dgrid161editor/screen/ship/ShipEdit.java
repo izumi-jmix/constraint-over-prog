3d0b1611269e66d6f4a4cf0160310c297ad05065
Aiden <a.izumi@haulmont.com>
2024-11-20 17:57:57 +0400

    Add possible solution

--- src/main/java/org/izumi/jmix/dgrid161editor/screen/ship/ShipEdit.java
+++ src/main/java/org/izumi/jmix/dgrid161editor/screen/ship/ShipEdit.java
@@ -2,8 +2,12 @@ package org.izumi.jmix.dgrid161editor.screen.ship;
 
 import java.util.Objects;
 
+import io.jmix.core.AccessManager;
+import io.jmix.ui.accesscontext.UiEntityAttributeContext;
 import io.jmix.ui.component.EntityPicker;
+import io.jmix.ui.component.Field;
 import io.jmix.ui.component.HasValue;
+import io.jmix.ui.component.data.meta.EntityValueSource;
 import io.jmix.ui.screen.EditedEntityContainer;
 import io.jmix.ui.screen.StandardEditor;
 import io.jmix.ui.screen.Subscribe;
@@ -20,11 +24,37 @@ import org.springframework.beans.factory.annotation.Autowired;
 public class ShipEdit extends StandardEditor<Ship> {
 
     @Autowired
+    private AccessManager accessManager;
+
+    @Autowired
     private EntityPicker<Port> currentPortField;
 
     @Subscribe("manufacturerField")
     public void onManufacturerFieldValueChange(final HasValue.ValueChangeEvent<Manufacturer> event) {
         final var value = event.getValue();
-        currentPortField.setEditable(Objects.nonNull(value));
+        setEditableConsideringAccess(currentPortField, Objects.nonNull(value));
+    }
+
+    private void setEditableConsideringAccess(Field<?> field, boolean editable) {
+        if (!editable) {
+            field.setEditable(false);
+            return;
+        }
+
+        final var valueSource = field.getValueSource();
+        if (!(valueSource instanceof EntityValueSource<?,?>)) {
+            //unable to determine user access
+            //log / throw exception
+            return;
+        }
+
+        final var entityValueSource = (EntityValueSource<?, ?>) valueSource;
+        final var propertyPath = entityValueSource.getMetaPropertyPath();
+
+        final var attributeContext = new UiEntityAttributeContext(propertyPath);
+        accessManager.applyRegisteredConstraints(attributeContext);
+        if (attributeContext.canModify()) {
+            field.setEditable(true);
+        }
     }
 }
